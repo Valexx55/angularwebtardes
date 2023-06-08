199d68c9e661a4d8fc49b1f9a50555923752372b
valexx55 <valexx55@gmail.com>
2023-06-08 21:25:09 +0200

    pendiente mostrar for dnis

--- myapp/src/app/components/dni/dni.component.ts
+++ myapp/src/app/components/dni/dni.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Dni } from 'src/app/models/dni';
 
 @Component({
   selector: 'app-dni',
@@ -7,6 +8,11 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 })
 export class DniComponent implements OnInit, OnDestroy {
 
+  //TODO: mostrar la lista dnis en la plantilla html
+  //ngFor directiva estructural
+
+
+
 
   titulo:string;//atributo de la clase
   valor:number | null; //UNION TYPE una variable puede ser de varios tipos
@@ -14,11 +20,14 @@ export class DniComponent implements OnInit, OnDestroy {
   static readonly SECUENCIA_LETRAS_DNI:string = "TRWAGMYFPDXBNJZSQVHLCKE";
   letra:string; //letra!:string;//! operador relaja la restricción de inicializar el atributo
 
+  listaDnis:Array<Dni>;
+
   constructor() { 
     //inicializamos
     this.titulo="CALCULO LETRA DNI";
     this.valor= null;
     this.letra='';
+    this.listaDnis = new Array<Dni>();//creo la lista vacía
   }
   ngOnDestroy(): void {
     console.log("ngOnDestroy DniComponent")
@@ -39,33 +48,58 @@ export class DniComponent implements OnInit, OnDestroy {
     if (this.valor!=null) //UNION TYPE si ya es un número...me meto
     {
       //sea dni español
-    if (inputSeleccionado.value!="sin")
-    {
-      //dni extranjero
-      //'035' X
-      //'135' Y
-      //'235' Z
-      let numeroString:string = inputSeleccionado.value + this.valor;
-      let numero:number = parseInt(numeroString);
-      let resto:number =  numero%DniComponent.SECUENCIA_LETRAS_DNI.length;
-      this.letra = DniComponent.SECUENCIA_LETRAS_DNI.charAt(resto);
+      if (inputSeleccionado.value!="sin")
+      {
+        //dni extranjero
+        //'035' X
+        //'135' Y
+        //'235' Z
+        let numeroString:string = inputSeleccionado.value + this.valor;
+        let numero:number = parseInt(numeroString);
+        let resto:number =  numero%DniComponent.SECUENCIA_LETRAS_DNI.length;
+        this.letra = DniComponent.SECUENCIA_LETRAS_DNI.charAt(resto);
 
+        let dni:Dni = new Dni();
+        dni.numero=this.valor;
+        dni.letra=this.letra;
+        dni.prefijo=inputSeleccionado.id;
+        this.listaDnis.push(dni);//add a la lista el dni
 
-    } else {
-      //dni español
-      //número % 23 --> posicion
-      let resto:number =  this.valor%DniComponent.SECUENCIA_LETRAS_DNI.length;
-      //posicion de SECUENCIA_LETRAS_DNI
-      this.letra = DniComponent.SECUENCIA_LETRAS_DNI.charAt(resto);
-      
+      } else {
+        //dni español
+        //número % 23 --> posicion
+        let resto:number =  this.valor%DniComponent.SECUENCIA_LETRAS_DNI.length;
+        //posicion de SECUENCIA_LETRAS_DNI
+        this.letra = DniComponent.SECUENCIA_LETRAS_DNI.charAt(resto);
+        
+        let dni:Dni = new Dni();
+        dni.numero=this.valor;
+        dni.letra=this.letra;
+        this.listaDnis.push(dni);//add a la lista el dni
 
-    }
+      }
     console.log("Letra DNI ESP = "+ this.letra );
-    //no
+      this.mostrarListaDnis();
+      this.mostrarListaDnisFuncional();
 
     }
     
 
+  }
+
+
+  mostrarListaDnis ():void
+  {
+    for (let i=0; i<this.listaDnis.length;i++)
+    {
+      console.log(`Dni = ${this.listaDnis[i].numero}  ${this.listaDnis[i].letra} ${this.listaDnis[i].prefijo}`);
+      //console.log("Dni = " + this.listaDnis[i].numero);//estilo clásico
+    }
+  }
+
+  mostrarListaDnisFuncional ():void
+  {
+    this.listaDnis.forEach(dni=> console.log(`Dni = ${dni.numero}  ${dni.letra} ${dni.prefijo}`));
   }
 
   dameEstilo(): string{
